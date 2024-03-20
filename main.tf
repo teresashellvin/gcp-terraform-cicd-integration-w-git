396f0e23925635f970235aab23c8ba77f2168362
Teresa Shellvin <teresashellvin@gmail.com>
2024-03-20 12:21:31 +0500

    main

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-provider "google" {
-  project = "elated-capsule-415713"
-  region  = "us-central1" # Replace with your desired region
-}
-
-
 resource "google_storage_bucket" "my-bucket" {
   name                     = "ts-terraform-git-cicd"
+  project                  = "elated-capsule-415713"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
